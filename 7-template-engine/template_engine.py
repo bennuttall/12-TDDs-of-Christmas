811347ce3d6d3adf2563ea1c70ccafbd4401c181
Ben Nuttall <ben@bennuttall.com>
2013-01-07 20:21:41 +0000

    Changed template evaluator to take a dictionary of variables instead of the object storing a single set of variables

--- 7-template-engine/template_engine.py
+++ 7-template-engine/template_engine.py
@@ -2,19 +2,13 @@ from re import findall
 
 
 class TemplateEngine:
-    def __init__(self, variables={}):
-        self.variables = variables
-
-    def add_variable(self, key, value):
-        self.variables[key] = value
-
-    def evaluate(self, string):
+    def evaluate(self, string, variables):
         vars_found = findall(r"\{([A-Za-z0-9_]+)\}", string)
         for var in vars_found:
             key = '{%s}' % var
-            if var not in self.variables:
+            if var not in variables:
                 raise TemplateEngine.MissingValueException
-            string = string.replace(key, self.variables[var])
+            string = string.replace(key, variables[var])
         return string
 
     class MissingValueException(Exception):
--- 7-template-engine/tests.py
+++ 7-template-engine/tests.py
@@ -5,47 +5,49 @@ import unittest
 class Test(unittest.TestCase):
     def setUp(self):
         self.engine = TemplateEngine()
-        self.engine.add_variable('name', 'Ben')
 
     def test_evaluate_string_with_no_variables(self):
         string = "Hello"
-        evaluated_string = self.engine.evaluate(string)
+        variables = {}
+        evaluated_string = self.engine.evaluate(string, variables)
         self.assertEqual(evaluated_string, "Hello")
 
     def test_evaluate_string_with_single_variable(self):
         string = "Hello {name}"
-        evaluated_string = self.engine.evaluate(string)
+        variables = {'name': 'Ben'}
+        evaluated_string = self.engine.evaluate(string, variables)
         self.assertEqual(evaluated_string, "Hello Ben")
 
     def test_evaluate_string_with_multiple_variables(self):
-        self.engine.add_variable('fname', 'Ben')
-        self.engine.add_variable('sname', 'Nuttall')
         string = "Hello {fname} {sname}"
-        evaluated_string = self.engine.evaluate(string)
+        variables = {
+                        'fname': 'Ben',
+                        'sname': 'Nuttall'
+                    }
+        evaluated_string = self.engine.evaluate(string, variables)
         self.assertEqual(evaluated_string, "Hello Ben Nuttall")
 
     def test_evaluate_string_with_non_existant_variable(self):
         string = "Hello {user}"
+        variables = {}
         method_call = self.engine.evaluate
         expected_exception = TemplateEngine.MissingValueException
-        self.assertRaises(expected_exception, method_call, string)
+        self.assertRaises(expected_exception, method_call, string, variables)
 
     def test_evaluate_string_with_double_curly_braces_around_variable(self):
         string = "Hello {{name}}"
-        evaluated_string = self.engine.evaluate(string)
+        variables = {'name': 'Ben'}
+        evaluated_string = self.engine.evaluate(string, variables)
         self.assertEqual(evaluated_string, "Hello {Ben}")
 
-    def test_initialise_template_engine_object_with_dictionary(self):
-        variables = {
-                        'this': 'THE',
-                        'is': 'STRING',
-                        'a': 'IS NOW',
-                        'string': 'CAPITALISED',
-                    }
-        new_engine = TemplateEngine(variables)
-        string = "{this} {is} {a} {string}"
-        expected_string = "THE STRING IS NOW CAPITALISED"
-        self.assertEqual(new_engine.evaluate(string), expected_string)
+    def test_evaluate_same_string_twice_with_different_variables(self):
+        string = "Hello {name}"
+        variables = {'name': 'Ben'}
+        evaluated_string_1 = self.engine.evaluate(string, variables)
+        variables = {'name': 'Bill'}
+        evaluated_string_2 = self.engine.evaluate(string, variables)
+        self.assertEqual(evaluated_string_1, "Hello Ben")
+        self.assertEqual(evaluated_string_2, "Hello Bill")
 
 if __name__ == '__main__':
     unittest.main()
