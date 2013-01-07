1fd16ba531ba04a1cff9928f79c09fdec3e8ff86
Ben Nuttall <ben@bennuttall.com>
2013-01-07 12:41:04 +0000

    Abstracted each sqaure's hint mapping into separate function

--- 3-mine-field/mine_field.py
+++ 3-mine-field/mine_field.py
@@ -6,7 +6,10 @@ class MineField:
 
     def hint(self):
         rows, cols = self.size
-        return "\n".join(''.join('*' if (r, c) in self.mines else self.count_neighbours(r, c) for c in range(cols)) for r in range(rows))
+        return "\n".join(''.join(self.square_hint(r, c) for c in range(cols)) for r in range(rows))
+
+    def square_hint(self, r, c):
+        return '*' if (r, c) in self.mines else self.count_neighbours(r, c)
 
     def count_neighbours(self, r, c):
         neighbours = [(r - 1, c - 1), (r - 1, c + 0), (r - 1, c + 1),
