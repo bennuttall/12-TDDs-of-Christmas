ce5b18da4b9c5b562bd1ec82157850596bad5288
Ben Nuttall <ben@bennuttall.com>
2013-02-12 01:16:27 +0000

    Error if hand is not 5 cards

---
+++ 12-harry-potter/tests.py
@@ -0,0 +1,11 @@
+from harry_potter import HarryPotter
+import unittest
+
+
+class Test(unittest.TestCase):
+    def test_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
