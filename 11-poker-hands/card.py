77dda55de9966f48a17d2f0b56c2daaad0346da5
Ben Nuttall <ben@bennuttall.com>
2013-02-16 16:17:00 +0000

    Moved Card class to separate file

---
+++ 11-poker-hands/card.py
@@ -0,0 +1,26 @@
+class Card:
+    values = ('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
+    suits = ('H', 'D', 'S', 'C')
+
+    def __init__(self, value, suit):
+        self.value = Card.validate_card_element(value, Card.values)
+        self.suit = Card.validate_card_element(suit, Card.suits)
+        self.value_index = Card.values.index(self.value)
+
+    def __str__(self):
+        return self.show()
+
+    def __repr__(self):
+        return self.show()
+
+    @staticmethod
+    def validate_card_element(element, group):
+        if element not in group:
+            raise Card.InvalidCardError
+        return element
+
+    def show(self):
+        return '%s%s' % (self.value, self.suit)
+
+    class InvalidCardError(Exception):
+        pass
--- 11-poker-hands/poker_hands.py
+++ 11-poker-hands/poker_hands.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from card import Card
 
 
 class PokerHand:
@@ -10,6 +11,12 @@ class PokerHand:
         if invalid_cards or duplicate_cards or len(cards) != 5:
             raise PokerHand.InvalidHandError
         self.cards = self.sort_cards_by_value(cards)
+
+    def __str__(self):
+        return str(self.show())
+
+    def __repr__(self):
+        return str(self.show())
 
     def show(self):
         return tuple(card.show() for card in self.cards)
@@ -42,7 +49,7 @@ class PokerHand:
         card_combinations = combinations(card_values, 2)
         repeated_values = [a for (a, b) in card_combinations if a == b]
         if len(repeated_values) == 2:
-            return ('Two Pair', tuple(repeated_values))
+            return ('Two Pairs', tuple(repeated_values))
         repeated_value = repeated_values[0]
         return self.which_n_of_a_kind(repeated_value)
 
@@ -89,37 +96,4 @@ class PokerHand:
         return sorted(cards, key=lambda card: card.value_index)
 
     class InvalidHandError(Exception):
-        pass
-
-
-class Card:
-    values = ('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2')
-    suits = ('H', 'D', 'S', 'C')
-
-    def __init__(self, value, suit):
-        self.value = Card.validate_card_element(value, Card.values)
-        self.suit = Card.validate_card_element(suit, Card.suits)
-        self.value_index = Card.values.index(self.value)
-
-    def __str__(self):
-        return self.show()
-
-    def __repr__(self):
-        return self.show()
-
-    @staticmethod
-    def validate_card_element(element, group):
-        if element not in group:
-            raise Card.InvalidCardError
-        return element
-
-    def show(self):
-        return '%s%s' % (self.value, self.suit)
-
-    class InvalidCardError(Exception):
-        pass
-
-
-class PokerGame:
-    def __init__(self, hand_1, hand_2):
         pass
