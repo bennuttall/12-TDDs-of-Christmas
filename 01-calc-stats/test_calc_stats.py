0ab67a4e81c3bbc961f357127c35239003d3df1a
Ben Nuttall <ben@bennuttall.com>
2018-12-01 13:53:40 +0000

    Use pytest

---
+++ 01-calc-stats/test_calc_stats.py
@@ -0,0 +1,10 @@
+from calc import Calculator
+
+def test_calc_stats():
+    values = [6, 9, 15, -2, 92, 11]
+    calc = Calculator(values)
+
+    assert calc.min == -2
+    assert calc.max == 92
+    assert calc.len == 6
+    assert calc.avg == 21.833333333333332
--- 01-calc-stats/tests.py
+++
@@ -1,9 +0,0 @@
-from calc import Calculator
-
-values = [6, 9, 15, -2, 92, 11]
-calc = Calculator(values)
-
-assert calc.min == -2
-assert calc.max == 92
-assert calc.len == 6
-assert calc.avg == 21.833333333333332
