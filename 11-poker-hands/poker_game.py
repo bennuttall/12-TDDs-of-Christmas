d0499cc9db67ee50da02556ea34bd683b1ee88e2
Ben Nuttall <ben@bennuttall.com>
2018-12-01 12:35:06 +0000

    Remove old solutions

--- 11-poker-hands/poker_game.py
+++
@@ -1,8 +0,0 @@
-class PokerGame:
-    def __init__(self, hand_1, hand_2):
-        self.hands = (hand_1, hand_2)
-
-    def winner(self):
-        winner = 1 if self.hands[0] > self.hands[1] else 2
-        winning_hand = sorted(self.hands).pop()
-        return 'Player %s wins - %s' % (winner, winning_hand.hand_name())
